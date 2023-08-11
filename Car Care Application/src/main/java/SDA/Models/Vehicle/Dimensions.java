4d34409dd282c639b2c39c1000ac95956f1d8b9f
mohamed wael <119164401+mhmedwael72@users.noreply.github.com>
2023-08-11 21:55:49 +0300

    model

---
+++ Car Care Application/src/main/java/SDA/Models/Vehicle/Dimensions.java
@@ -0,0 +1,29 @@
+package SDA.Models.Vehicle;
+
+public class Dimensions {
+
+    double length;
+    double width;
+
+    public Dimensions() {
+        length = 0;
+        width = 0;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+}
